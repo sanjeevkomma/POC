f7dd80eb74a2de173dd1a01756c9c11467f6e351
sanjeev <sanjeevkomma@gmail.com>
2025-09-28 13:17:58 +0530

    in

--- user-management-system/user-management-service/README.md
+++ user-management-system/user-management-service/README.md
@@ -1,23 +1,18 @@
 ## Technology Stack :    
 
-   1. Java 8
-   2. Spring Boot 2.3.0.M1
-   3. H2 DB
+   1. Java 21
+   2. Spring Boot 3
+   3. MySQL
 
 ## Application Design : 
 
 ## Commands : 
 
-   1. mvn clean install
-   2. mvn eclipse:clean
-   3. mvn eclipse:eclipse
+
 
 ## How to Start : 
 
-    1. git clone https://github.com/sanjeevkomma/spring-boot.git
-    2. mvn spring-boot:run
-    3. Access H2 Console ( http://localhost:8080/h2-console/login.jsp ) 
-    
+   
 ## REST API : 
 
     1. Get All User : GET : http://localhost:8080/user
--- user-management-system/user-management-service/src/main/java/com/demo/model/User.java
+++ user-management-system/user-management-service/src/main/java/com/demo/model/User.java
@@ -4,9 +4,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity  
-@Table  
+@Entity
+@Table(name="users")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User   {
 
 	@Id  
@@ -18,43 +26,4 @@ public class User   {
 
 	@Column
 	private String email;
-
-	
-	public User() {
-	}
-	
-	
-	public User(int id, String name, String email) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.email = email;
-	}
-
-
-	public int getId()   { 
-
-		return id;  
-	}  
-	public void setId(int id)  { 
-
-		this.id = id;  
-	}  
-	public String getName()   { 
-
-		return name;  
-	}  
-	public void setName(String name)   {  
-
-		this.name = name;  
-	}  
-
-	public String getEmail() {    
-
-		return email;  
-	}  
-	public void setEmail(String email)   {
-
-		this.email = email;  
-	}  
 }  
