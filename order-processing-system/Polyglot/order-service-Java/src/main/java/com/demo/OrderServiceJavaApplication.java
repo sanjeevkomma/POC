87b1c84e86174ef4eca03887fe551426190329a3
sanjeev <sanjeevkomma@gmail.com>
2025-12-31 23:45:49 +0530

    in

---
+++ order-processing-system/Polyglot/order-service-Java/src/main/java/com/demo/OrderServiceJavaApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceJavaApplication.class, args);
+	}
+
+}
